1ba2cac94910c8184533f68a0e12b967c8cf1c63
david <d.phi.gordon@gmail.com>
2023-04-02 15:13:58 -0500

    added example 2

---
+++ example-2/README.md
@@ -0,0 +1,1 @@
+based on [https://fla.my]
---
+++ example-2/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="text-3xl font-bold underline">
+				blank project using react, typescript, vite, and tailwind
+    </div>
+  )
+}
+
+export default App
