8ecd377271d317bc3d5054f86ba6e8f5955d40dd
David <d.phi.gordon@gmail.com>
2023-04-03 17:42:24 -0500

    updated homepage styles

--- davidgordon.dev/src/App.tsx
+++ davidgordon.dev/src/App.tsx
@@ -23,35 +23,87 @@ function App() {
             <li>
               <Link href="https://www.underengineered.dev">hobby blog</Link>
             </li>
+            <li>
+              <Link href="https://www.linkedin.com/in/david-gordon-71793926a">
+                linked-in
+              </Link>
+            </li>
           </ul>
         </div>
       </nav>
-      <main className="px-4 mt-8 mb-8">
-        <header className="mt-20 mb-60">
-          <H1>David Gordon, Software Engineer</H1>
-          <Prose>
-            I'm a software engineer focused on frontend and backend development
-            of stable, scalable web technologies. I'm currently open for
-            full-time and contract development work. My technical and
-            professional interests also include System Programming, Game
-            Programming, Math, and All Things Linux.
-          </Prose>
-          <Prose>
-            <Link underline href="mailto:gordopdavid@gmail.com">
-              gordopdavid@gmail.com
-            </Link>
-          </Prose>
-        </header>
+      <header className="mt-20 mb-60 max-w-screen-sm mx-auto">
+        <H1>David Gordon, Software Engineer</H1>
+        <Prose>
+          I'm a software engineer focused on frontend and backend development of
+          stable, scalable web technologies. I'm currently open for full-time
+          and contract development work. My technical and professional interests
+          also include System Programming, Game Programming, Math, and All
+          Things Linux.
+        </Prose>
+        <Prose>
+          <Link underline href="mailto:gordopdavid@gmail.com">
+            gordopdavid@gmail.com
+          </Link>
+        </Prose>
+      </header>
+      <main className="px-4 mt-8 mb-8 max-w-screen-sm mx-auto">
         <section className="space-y-2">
           <H1>Featured Projects</H1>
-          <div className="bg-black w-full h-96"></div>
-          <div className="bg-black w-full h-96"></div>
-          <div className="bg-black w-full h-96"></div>
+          <ProjectCard
+            title="Chat Application"
+            description="An anonymous chat application built using a Java Spring backend, a PostgreSQL database, and a React client. The backend is fully containerized and running in an AWS EC2 instance."
+            source={{
+              href: "https://github.com/gordon-david/chat",
+              text: "source",
+            }}
+            demo={{ href: "https://chat.underengineered.dev", text: "demo" }}
+            img={{ src: "/static/chat-screenshot.png" }}
+          />
+										<ProjectCard 
+										title="Photographer (CMS Backed Website)" 
+										description="A simple website built with a React client and a Strapi CMS backend. The goal was to provide a simple site for a photographer that allows the client to manage the images on the site without developer intervention. The backend may be easily deployed as a container."
+										source={{href: "https://github.com/gordon-david/photographer", text:"source"}}
+										img={{src:"/static/photographer-screenshot.png"}}
+										/>
         </section>
       </main>
     </div>
   );
 }
+
+const ProjectCard = (props: {
+  img?: { src: string };
+  title: string;
+  description: string;
+  source?: { href: string; text: string };
+  demo?: { href: string; text: string };
+}) => (
+  <div className="w-full border-2 border-black py-1 px-1">
+    <div className="w-full h-72 sm:h-80 md:h-96 bg-gray-300">
+      {props.img ? (
+        <img
+          className="max-w-sm mx-auto w-full h-full object-top object-cover border-2 border-gray-300 bg-gray-300"
+          src={props.img.src}
+          alt=""
+        />
+      ) : (
+        <div className="bg-black w-full h-full"> </div>
+      )}
+    </div>
+    <h3 className="text-center">{props.title}</h3>
+    {props.source ? (
+      <Link href={props.source.href} display="block">
+        {props.source.text}
+      </Link>
+    ) : null}
+    {props.demo ? (
+      <Link href={props.demo.href} display="block">
+        {props.demo.text}
+      </Link>
+    ) : null}
+    <Prose>{props.description}</Prose>
+  </div>
+);
 
 /* UTILITY */
 
@@ -60,16 +112,19 @@ const H1 = (props: { children: string }) => (
 );
 
 const Prose = (props: { children: ReactNode }) => (
-  <p className="mb-4">{props.children}</p>
+  <p className="mb-4 mt-1">{props.children}</p>
 );
 
 const Link = (props: {
   children: ReactNode;
   underline?: boolean;
   href: string;
+  display?: "block" | "inline";
 }) => (
   <a
-    className={`${props.underline ? "underline " : ""}hover:text-red-600`}
+    className={`${props.underline ? "underline " : ""} ${
+      props.display && props.display + " "
+    } text-red-900 hover:text-red-500`}
     href={props.href}
   >
     {props.children}
